982a9deea091f913e20e3ba1ae5afd6a0ad1e22f
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2023-02-23 02:16:04 +0900

    project start

---
+++ src/components/Orders.jsx
@@ -0,0 +1,74 @@
+import { useContext, useMemo } from "react";
+import AppStateContext from "../contexts/AppStateContext";
+import useActions from "../hooks/useActions";
+
+export default function Orders() {
+  const { orders } = useContext(AppStateContext);
+  const { prototypes } = useContext(AppStateContext);
+
+  const { remove, removeAll } = useActions();
+
+  const totalPrice = useMemo(() => {
+    return orders
+      .map((order) => {
+        const { id, quantity } = order;
+        const prototype = prototypes.find((p) => p.id === id);
+        return prototype.price * quantity;
+      })
+      .reduce((l, r) => l + r, 0);
+  }, [orders, prototypes]);
+
+  return (
+    <aside>
+      <div className="order">
+        <div className="body">
+          {orders.map((order) => {
+            const { id } = order;
+            const prototype = prototypes.find((p) => p.id === id);
+            const click = () => {
+              remove(id);
+            };
+            return (
+              <div className="item" key={id}>
+                <div className="img">
+                  <video src={prototype.thumbnail} />
+                </div>
+                <div className="content">
+                  <p className="title">
+                    {prototype.title} X {order.quantity}
+                  </p>
+                </div>
+                <div className="action">
+                  <p className="price">$ {prototype.price * order.quantity}</p>
+                  <button className="btn btn--link" onClick={click}>
+                    <i className="icon icon--cross" />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="total">
+          <hr />
+          <div className="item">
+            <div className="content">Total</div>
+            <div className="action">
+              <div className="price">$ {totalPrice}</div>
+            </div>
+            <button className="btn btn--link" onClick={removeAll}>
+              <i className="icon icon--delete" />
+            </button>
+          </div>
+          <button
+            className="btn btn--secondary"
+            style={{
+              width: "100%",
+              marginTop: 10,
+            }}>
+            Checkout
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
+}
